efaa9629e37a1d2bfcecf3d5e94121ea92f011b4
lomshakov <lomshakov88@gmail.com>
2020-12-07 01:49:33 +0700

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://wowslider.com/sliders/demo-34/data1/images/matterhorn1364829.jpg'/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
